81e909e9031d8f8a68101080f225b4a977ec0015
Matthias Andreas Benkard <code@mail.matthias.benkard.de>
2012-10-09 11:31:07 +0200

    Make use of Module::Build for the build script.

---
+++ lib/Net/MulkyID/Builder.pm
@@ -0,0 +1,24 @@
+#! /usr/bin/env perl
+
+package Net::MulkyID::Builder;
+
+use Module::Build;
+
+our @ISA = 'Module::Build';
+
+sub new {
+  my ($class, @args) = @_;
+  my $self = $class->SUPER::new(@args);
+  return bless $self, $class;
+  #my $self = $self->SUPER::new();
+  #return $self;
+  #return bless {}, shift;
+}
+
+sub ACTION_build {
+  my ($self, @args) = @_;
+  eval "use Net::MulkyID::Setup; setup();";
+  #return SUPER::ACTION_build($self, @args);
+}
+
+1;
